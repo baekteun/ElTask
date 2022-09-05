0d138c0220d65aa0a48c8fd3de7079ce380b7955
baegteun <baegteun@gmail.com>
2022-09-05 21:53:00 +0900

    Initial Commit

---
+++ ElTask/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ElTask
+//
+//  Created by 최형우 on 2022/09/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ElTask/ElTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElTaskApp.swift
+//  ElTask
+//
+//  Created by 최형우 on 2022/09/05.
+//
+
+import SwiftUI
+
+@main
+struct ElTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
